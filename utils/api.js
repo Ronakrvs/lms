2db5dabb308f3847de27a73dab5205b5246988aa
ronak <ronak@hoicko.net>
2023-09-04 16:10:44 +0530

    change

--- utils/api.js
+++ utils/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const httpClient = axios.create({
-  baseURL: `http://15.206.144.233:5000/api`,//YOUR_API_URL HERE
+  // baseURL: `http://15.206.144.233:5000/api`,//YOUR_API_URL HERE
   // baseURL: `http://localhost:4002/api`, //YOUR_API_URL HERE
+  baseURL: `https://backend-lms-zeta.vercel.app/api`, //YOUR_API_URL HERE
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json, text/plain, */*',
